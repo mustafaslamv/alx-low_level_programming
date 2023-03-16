e0879b90b14f64d2b04b365225d95a6242acf3c1
Mustafa Slama <ayman1slama@gmail.com>
2023-03-16 17:05:54 +0200

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /* 4.Hello, puts  */
 int main(void)
 {
-	char *string = ""Programming is like building a multilingual puzzle";
+	char *string = "\"Programming is like building a multilingual puzzle";
 	puts(string);
 	return 0;
 }
