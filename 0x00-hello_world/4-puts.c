1e67a8cea2cc112c0d07b421418f8a540db0ecb1
Mustafa Slama <ayman1slama@gmail.com>
2023-03-16 17:08:52 +0200

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /* main function to print string using puts func */
 int main(void)
 {
-	char *string = "\"Programming is like building a multilingual puzzle";
+	char *string = "\"Programming is like building a multilingual puzzle\n";
 	
 	puts(string);
 	return (0);
