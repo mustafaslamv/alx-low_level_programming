7a0beec9a4df372c11aea0d9bb38449603c4f003
Mustafa Slama <ayman1slama@gmail.com>
2023-03-22 00:33:43 +0200

    103 fib

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int i;
+	unsigned long first = 0, second = 1, sum, totalsum;
+
+	for (i = 0; i < 50; i++)
+	{
+		sum = first + second;
+		first = second;
+		second = sum;
+
+		if (sum % 2 == 0)
+		{
+			totalsum += sum;
+		}
+		printf("%lu\n", totalsum);
+
+	}
+}
