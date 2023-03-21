d92a9301d9a46d7e68eb2fbeafaa3d5c4bf565d0
Mustafa Slama <ayman1slama@gmail.com>
2023-03-21 19:51:54 +0200

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * table - function definition
+ */
+
+void table(void)
+{
+	int j, i, d;
+
+	for (j = 0; j <= 9; j++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (i = 1; i <= 9; i++)
+		{
+			d = (j * i);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (i < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+}
