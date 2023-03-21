3f054d4944de6c4da7fae703aa567d30a7a52835
Mustafa Slama <ayman1slama@gmail.com>
2023-03-21 15:28:30 +0200

    check for lowerC func

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,14 +5,14 @@
  * @c: character ascii code
  */
 
-void _islower(int c)
+int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		_putchar('1');
+		return (1);
 	}
 	else
 	{
-		_putchar('0');
+		return (0);
 	}
 }
