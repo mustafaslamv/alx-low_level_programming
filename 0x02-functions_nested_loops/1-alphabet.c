3278c7c581e68dde832cd6767080791a51f015f2
Mustafa Slama <ayman1slama@gmail.com>
2023-03-21 15:10:15 +0200

    task: print alphabet by their ascii values

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,6 @@
 /**
  * print_alphabet - function definition
  *
- * Return: 0
  */
 
 void print_alphabet(void)
@@ -15,5 +14,4 @@ void print_alphabet(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
