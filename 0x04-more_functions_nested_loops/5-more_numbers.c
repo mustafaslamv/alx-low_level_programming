2af4aadedbca5e6ebfd9ce30f6407bed19dcf6f8
Mustafa Slama <ayman1slama@gmail.com>
2023-03-23 16:29:30 +0200

    print numbers using putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 14  using putchar
+ */
+
+void print_most_numbers(void)
+{
+	int i, j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar('0' + (i / 10));
+			}
+			_putchar('0' + (i % 10));
+		}
+		_putchar('\n');
+	}
+}
