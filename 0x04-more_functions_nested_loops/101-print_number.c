be6eaf3ac580f97e92bdcce0ec51cf8d57a222de
Mustafa Slama <ayman1slama@gmail.com>
2023-03-23 23:16:37 +0200

    print function

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - print integer using putchar
+ *@n: input number
+ */
+void print_number(int n)
+{
+
+	unsigned int result = n;
+
+	if (n < 0)
+	{
+		_putchar('-';
+		result = -result;
+	}
+
+	if ((result / 10) > 0)
+	{
+		print_number(result / 10);
+	}
+	_putchar((num % 10) + 48);
+}
