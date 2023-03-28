f94c5218e1b7724068e997bd089b61675a128299
Mustafa Slama <ayman1slama@gmail.com>
2023-03-28 03:04:43 +0200

    task 1: change to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to change var value by its address
+ * @n: variable we want to change it's value to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
