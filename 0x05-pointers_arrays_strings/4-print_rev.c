f1921c8cb0742e54781fb6821dd2e7033fc6e5b8
Mustafa Slama <ayman1slama@gmail.com>
2023-03-28 03:50:14 +0200

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - function to print string in reverse order
+ * @s: input string
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*s != 0)
+	{
+		length++;
+		s++;
+	}
+
+	while (length != 1)
+	{
+		_putchar(*s);
+		lenght--;
+		s--;
+	}
+	_putchar('\n');
+
+}
+
+
