12fcd0a3749ace5724707108f42cd5890227dd9d
Mustafa Slama <ayman1slama@gmail.com>
2023-04-03 21:36:31 +0200

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,16 +11,16 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, result = 0;
 	char *t = accept;
-	
+
 	for (i = 0; t[i] != '\0'; i++)
 	{
 		for (j = 0; s[j] != '\0'; j++)
 		{
-			if ((t[i] == s[j]))
+			if (t[i] == s[j])
 			{
 				t++;
 				result++;
-				continue;;
+				continue;
 			}
 		}
 		if (t[j] == '\0')
