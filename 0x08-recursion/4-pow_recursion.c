dd957da0e0b0ff25ee9586a27d048fe86339a92d
Mustafa Slama <ayman1slama@gmail.com>
2023-04-05 20:28:52 +0200

    status

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - recursive power function
+ *
+ * @x: base number
+ * @y: power raised
+ * Return: (y < 0) >> -1 || (y == 0) >> 1 || x * _pow_recursion(x, y - 1)
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
