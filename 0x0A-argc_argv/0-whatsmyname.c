15a68e2c53e110c7475d76ab08899196e93efe7c
mustafa slama <ayman1slama@gmail.com>
2023-04-09 18:15:22 +0200

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments passed
+ * @argv: array of arguments passed
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
