d18de9e2dfb98698a7c0cd1a394a00b7bc8d97c8
mustafa slama <ayman1slama@gmail.com>
2023-04-09 22:56:47 +0200

    print argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments passed
+ * @argv: array of arguments passed
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
