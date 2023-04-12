41ea3c4f3d360f9ad41b7b210395843a2f7a9545
mustafa slama <ayman1slama@gmail.com>
2023-04-12 04:54:36 +0200

    concat 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1[0] = '\0';
+		s1= "\0";
 	}
 	else if (s2 == NULL)
 	{
-		s2[0] = '\0';
+		s2 = "\0";
 	}
 
 	while (s1[len1] != '\0')
