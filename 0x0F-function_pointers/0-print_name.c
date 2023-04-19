6ccb7685042ace8d810f78d268002ffb1205bbdd
mustafa slama <ayman1slama@gmail.com>
2023-04-20 00:14:37 +0200

    create task files && task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: pointer to string
+ * @f: pointer to function
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
