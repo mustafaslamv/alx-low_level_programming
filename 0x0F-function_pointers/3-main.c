68300de2a5cb6829908e17601959906b05a38f45
mustafa slama <ayman1slama@gmail.com>
2023-04-20 03:38:20 +0200

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: arguments counter
+ * @argv: arguments values
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*op_func)(int, int);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - performs simple operations
+ * @a: first number
+ * @b: second number
+ * Return: result of the simple operation
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - performs simple operations
+ * @a: first number
+ * @b: second number
+ * Return: result of the simple operation
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - performs simple operations
+ * @a: first number
+ * @b: second number
+ * Return: result of the simple operation
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - performs simple operations
+ * @a: first number
+ * @b: second number
+ * Return: result of the simple operation
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - performs simple operations
+ * @a: first number
+ * @b: second number
+ * Return: result of the simple operation
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
