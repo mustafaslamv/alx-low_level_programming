fe7c27de9d0716ac1f65274657b124802eaf3de0
mustafa slama <ayman1slama@gmail.com>
2023-04-20 03:53:37 +0200

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 	int (*op_func)(int, int);
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
@@ -18,20 +19,20 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 	op_func = get_op_func(argv[2]);
 
 	if (op_func == NULL)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 
 	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 
 	result = op_func(num1, num2);
