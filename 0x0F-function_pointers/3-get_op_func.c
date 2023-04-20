495a405841800fd6cf35190868ff1681f8f4da65
mustafa slama <ayman1slama@gmail.com>
2023-04-20 04:05:26 +0200

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <string.h>
 
 /**
  * get_op_func - function selects the operation
@@ -13,14 +14,15 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}};
+		{NULL, NULL}
+	};
 
 	int i;
 
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (s && s[0] == ops[i].op[0] && !s[1])
+		if (strcmp(ops[i].op, s) == 0)
 		{
 			return ((ops[i].f));
 		}
