77b9665a708adad8d18fc581a1d118dbd7f31595
mustafa slama <ayman1slama@gmail.com>
2023-04-21 02:21:36 +0200

    ISO C90 fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,21 +1,19 @@
 #include "variadic_functions.h"
-
 /**
  * sum_them_all - function that returns the sum of all its parameters
  * @n: number of items passed to the function
  * Return: sum
 */
-
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0, i;
+	int sum = 0;
+	unsigned int i;
+	va_list args;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-
-	va_list args;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
