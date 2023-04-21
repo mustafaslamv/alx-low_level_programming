f5db56036202bd4c5de901df34d8c46a2f53f8dd
mustafa slama <ayman1slama@gmail.com>
2023-04-21 03:20:32 +0200

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,93 @@
+#include "variadic_functions.h"
+
+
+/**
+ * print_char - print float from var args
+ *
+ * @args: args to print from
+ *
+ * Return: void
+ */
+void print_char(va_list args)
+{
+	printf("%c", (char) va_arg(args, int));
+}
+
+/**
+ * print_int - print int from var args
+ *
+ * @args: args to print from
+ *
+ * Return: void
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - print float from var args
+ *
+ * @args: args to print from
+ *
+ * Return: void
+ */
+void print_float(va_list args)
+{
+	printf("%f", (float) va_arg(args, double));
+}
+
+/**
+ * print_str - print str from var args
+ *
+ * @args: args to print from
+ *
+ * Return: void
+ */
+void print_str(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char*);
+	if (s == NULL)
+	{
+        printf("(nil)");
+    }
+	else
+	{
+        printf("%s", s);
+    }
+}
+
+
+/**
+ * print_all -  prints anything.
+ * @format: list of types of arguments passed to the function
+ */
+void print_all(const char* const format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    op_t ops[] = {
+        {'c', print_char},
+        {'i', print_int},
+        {'f', print_float},
+        {'s', print_str},
+    };
+
+    size_t num_ops = sizeof(ops) / sizeof(op_t);
+    for (size_t i = 0; format[i] != '\0'; i++) {
+        for (size_t j = 0; j < num_ops; j++) {
+            if (format[i] == ops[j].type) {
+                ops[j].print_fn(args);
+                if (format[i + 1] != '\0') {
+                    printf(", ");
+                }
+                break;
+            }
+        }
+    }
+
+    printf("\n");
+    va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct op - select what operation to do
+ * @f: operation
+*/
+typedef struct op
+{
+    char type;
+    void (*print_fn)(va_list);
+} op_t;
+
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
