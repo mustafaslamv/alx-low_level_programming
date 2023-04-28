717b90f76b254d333b16e0ec03d05a4731326570
mustafa slama <ayman1slama@gmail.com>
2023-04-28 06:49:46 +0300

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: start node
+ */
+
+void free_list(list_t *head)
+{
+	list_t *ptr = head;
+	list_t *next;
+
+	while (ptr != NULL)
+	{
+		next = ptr->next;
+		free(ptr->str);
+		free(ptr);
+		ptr = next;
+	}
+}
