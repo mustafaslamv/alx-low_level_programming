985e791895a9fa1c2325b8bab4452310be1ed3c5
mustafa slama <ayman1slama@gmail.com>
2023-05-05 03:07:10 +0300

    replace pow with << operator

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,6 @@
 #include "main.h"
 
 /**
- * power - simple power function
- * @base: base
- * @value: power
- * Return: unsigned int result
- */
-unsigned int power(int base, int value)
-{
-	unsigned int result = 1;
-
-	if (value == 0)
-	{
-		return (1);
-	}
-
-	while (value > 0)
-	{
-		result *= base;
-		value--;
-	}
-	return (result);
-}
-
-/**
  * binary_to_uint - function that converts a binary number to an unsigned int
  * @b: binary number as a string
  * Return: the converted number || 0
@@ -47,7 +24,7 @@ unsigned int binary_to_uint(const char *b)
 		}
 		else if (b[i] == '1')
 		{
-			result += power(2, (length - i - 1));
+			result += 1 << (length - i - 1);
 		}
 		else
 		{
